93f037b01d6239987522d844508bfdf57db85087
Ayonete <63324902+Ayonete@users.noreply.github.com>
2025-04-12 10:50:40 +0100

    logo fix

--- greenhouse/urls.py
+++ greenhouse/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('greenhouse/', permanent =False)),
     path("admin/", admin.site.urls),
     path("greenhouse/", include("crops.urls")),
 ]
